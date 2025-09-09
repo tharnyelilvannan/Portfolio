442346ef7cc0b49f2b33ca453830f33ac752dfe6
tharnyelilvannan <tharny2005@outlook.com>
2025-09-09 18:10:03 -0400

    Finish home page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -5,8 +5,45 @@ import { motion, AnimatePresence } from "motion/react"
 
 export default function Page() {
     return (
-        <div className="background">
+        <div className="projectBackground">
+            <div className='flex flex-row justify-evenly'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    className="box bg-gray-300"
+                    transition={{
+                        duration: 0.4,
+                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.7 },
+                    }}></motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    className="box bg-gray-300"
+                    transition={{
+                        duration: 0.4,
+                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.7 },
+                    }}></motion.div>
 
+
+            </div>
+            <div className='flex flex-row justify-evenly'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    className="box bg-gray-300"
+                    transition={{
+                        duration: 0.4,
+                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.7 },
+                    }}></motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    className="box bg-gray-300"
+                    transition={{
+                        duration: 0.4,
+                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.7 },
+                    }}></motion.div>
+            </div>
         </div>
     )
 }
